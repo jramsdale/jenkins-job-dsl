8418be9504457b6bc119c3e5aa06984afbf29aae
jmusselwhite <jmusselwhite@google.com>
2018-08-13 14:09:35 -0700

    Adding job parameters

--- gke_stateful_applications_demo.groovy
+++ gke_stateful_applications_demo.groovy
@@ -1,4 +1,10 @@
 pipelineJob('job-dsl-pipeline-test-again') {
+
+    parameters {
+        stringParam('CLUSTER_ZONE', 'us-west1-c', 'The zone to build the test cluster in')
+        stringParam('PROJECT_ID', 'pso-helmsman-cicd', 'The project to build the test cluster in')
+        stringParam('REGION', 'us-west1', 'The region to build the test cluster in')
+    }
     definition {
         cpsScm {
             scm {
