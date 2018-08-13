7faa6b3f47001e3e248968b74c634dbd4d1e9eb6
jmusselwhite <jmusselwhite@google.com>
2018-08-13 14:16:02 -0700

    Adding more Git properties

--- gke_stateful_applications_demo.groovy
+++ gke_stateful_applications_demo.groovy
@@ -1,14 +1,27 @@
 pipelineJob('job-dsl-pipeline-test-again') {
 
+    properties {
+        githubProjectUrl('https://github.com/GoogleCloudPlatform/gke-stateful-appliations-demo')
+    }
+
     parameters {
         stringParam('CLUSTER_ZONE', 'us-west1-c', 'The zone to build the test cluster in')
         stringParam('PROJECT_ID', 'pso-helmsman-cicd', 'The project to build the test cluster in')
         stringParam('REGION', 'us-west1', 'The region to build the test cluster in')
+        stringParam('sha1', '', '')
     }
     definition {
         cpsScm {
             scm {
-                git('https://github.com/GoogleCloudPlatform/gke-stateful-applications-demo.git')
+                git {
+                    branch('${sha1}')
+                    remote {
+                        name('origin')
+                        refspec('+refs/pull/*:refs/remotes/origin/pr/*')
+                        url('https://github.com/GoogleCloudPlatform/gke-stateful-applications-demo.git')
+                    }
+                }
+
             }
         }
     }
