0ed63ebf63004cec0319888343837c70f55250f6
jmusselwhite <jmusselwhite@google.com>
2018-08-14 13:24:46 -0700

    Making an easy to follow template

--- gke_stateful_applications_demo.groovy
+++ gke_stateful_applications_demo.groovy
@@ -1,19 +1,26 @@
-pipelineJob('job-dsl-pipeline-test-again') {
+PROJECT_NAME = 'gke-stateful-applications-demo'
+PROJECT_URL = "https://github.com/GoogleCloudPlatform/${PROJECT_NAME}"
+GIT_URL = "${PROJECT_URL}.git"
+// The name of your job in Jenkins
+pipelineJob(PROJECT_NAME) {
 
+    // The URL for your project page. Does not include .git
     properties {
-        githubProjectUrl('https://github.com/H3Lm5m4n/gke-stateful-applications-demo')
+        githubProjectUrl(PROJECT_URL)
     }
 
+    // These will likely be different per demo. Change these as needed
     parameters {
         stringParam('CLUSTER_ZONE', 'us-west1-c', 'The zone to build the test cluster in')
         stringParam('PROJECT_ID', 'pso-helmsman-cicd', 'The project to build the test cluster in')
         stringParam('REGION', 'us-west1', 'The region to build the test cluster in')
     }
 
+    // Make sure GoogleCloudPlatform is whitelisted
     triggers {
         githubPullRequest {
             useGitHubHooks()
-            orgWhitelist('H3Lm5m4n')
+            orgWhitelist('GoogleCloudPlatform')
         }
     }
 
@@ -26,7 +33,7 @@ pipelineJob('job-dsl-pipeline-test-again') {
                     remote {
                         name('origin')
                         refspec('+refs/pull/${ghprbPullId}/*:refs/remotes/origin/pr/${ghprbPullId}/*')
-                        url('https://github.com/H3Lm5m4n/gke-stateful-applications-demo.git')
+                        url(GIT_URL)
                     }
                 }
 
